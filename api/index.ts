5b6739e919fcce4a9dd7f1c2035614ce56b4f7cd
Dominik Meister <dominik.s.meister@gmail.com>
2024-11-14 15:37:28 +0100

    added port 3000

--- api/index.ts
+++ api/index.ts
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${3000}`)
 })
