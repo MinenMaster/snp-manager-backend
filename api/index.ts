d7b9bdc8838f570cca06e4d0d51622f219756493
Dominik Meister <dominik.s.meister@gmail.com>
2024-11-14 15:52:18 +0100

    got express to work

--- api/index.ts
+++ api/index.ts
@@ -9,20 +9,12 @@ const app = express();
 app.use(express.json());  
 app.use(cors());
 
+app.listen(3000, () => console.log("Server ready on port 3000.")); //! This stuff is useless, I think
+
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "BaseURL!" });
+    res.json({ message: "This is the SNP-Manager-API base url." });
 });
 
 app.get("/api", (req: Request, res: Response) => {
     res.json({ message: "Hello from Express API with TypeScript!" });
 });
-
-app.listen(3000, () => console.log("Server ready on port 3000.")); //! This stuff is useless
-
-app.get("/passwords", (req: Request, res: Response) => {
-  res.send("Getting passwords...");
-});
-
-// app.listen(3000, () => {
-//   console.log(`Example app listening on port ${3000}`)
-// })
