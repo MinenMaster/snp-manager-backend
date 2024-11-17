2f0a657edf0e6215fcc7456e86c555e0425ff19f
Dominik Meister <dominik.s.meister@gmail.com>
2024-11-17 16:34:56 +0100

    added a separate register component

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { sql } from "@vercel/postgres";
 import moment from "moment-timezone";
+import { registerUser } from "./register";
 
 var cors = require("cors");
 const app = express();
@@ -71,29 +72,4 @@ const logLogin = async (username: string) => {
     }
 };
 
-app.post("/register", async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-
-    if (!username || !password) {
-        return res
-            .status(400)
-            .json({ message: "Username and password are required" });
-    }
-
-    try {
-        const existingUser =
-            await sql`SELECT * FROM snp_users WHERE username = ${username}`;
-        if (existingUser.rows.length > 0) {
-            return res.status(400).json({ message: "Username already exists" });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await sql`INSERT INTO snp_users (username, hashed_password) VALUES (${username}, ${hashedPassword})`;
-
-        res.status(201).json({ message: "User registered successfully" });
-    } catch (err) {
-        console.error("Error registering user:", err);
-        res.status(500).json({ message: "Internal server error" });
-    }
-});
+app.post("/register", registerUser);
