8744baf6e8b71db1b27d91ce5fef1f26e71f2b61
Dominik Meister <dominik.s.meister@gmail.com>
2024-11-17 17:23:01 +0100

    commented out .listen()

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => console.log("Server ready on port 3000.")); //! This stuff is useless, I think
+// app.listen(3000, () => console.log("Server ready on port 3000.")); //! This stuff is useless, I think
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: "This is the SNP-Manager-API base url." });
