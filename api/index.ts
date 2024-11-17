8a80979a3acbc55b1514aa402afc89e5bcef4357
Dominik Meister <dominik.s.meister@gmail.com>
2024-11-17 16:11:24 +0100

    added login

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { sql } from "@vercel/postgres";
+import moment from "moment-timezone";
 
 var cors = require("cors");
 const app = express();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(express.json());
 app.use(cors());
@@ -14,3 +17,56 @@ app.listen(3000, () => console.log("Server ready on port 3000.")); //! This stuf
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: "This is the SNP-Manager-API base url." });
 });
+
+app.post("/login", async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ message: "Username and password are required" });
+    }
+
+    const { rows } =
+        await sql`SELECT * FROM snp_users WHERE username = ${username};`;
+
+    if (rows.length > 0) {
+        const user = rows[0];
+        const isValidPassword = await bcrypt.compare(
+            password,
+            user.hashed_password
+        );
+
+        if (isValidPassword) {
+            const token = jwt.sign({ username: user.username }, JWT_SECRET, {
+                expiresIn: "1h",
+            });
+
+            await logLogin(user.username);
+
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: "Invalid credentials" });
+        }
+    } else {
+        res.status(401).json({ message: "Invalid credentials" });
+    }
+});
+
+const logLogin = async (username: string) => {
+    const timeZone = "Europe/Zurich";
+
+    const timestampISO = moment().tz(timeZone).format("YYYY-MM-DD HH:mm:ss");
+
+    const timestampFormatted = moment()
+        .tz(timeZone)
+        .format("DD.MM.YYYY HH:mm:ss (Z)");
+
+    console.log(`[${timestampFormatted}] User ${username} logged in.`);
+
+    try {
+        await sql`INSERT INTO snp_login_logs (username, timestamp) VALUES (${username}, ${timestampISO});`;
+    } catch (err) {
+        console.error("Error logging login to database:", err);
+    }
+};
