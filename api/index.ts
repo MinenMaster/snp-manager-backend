4172bf19c5f2d1fd43ed70fa06d079bf23bbae90
Dominik Meister <dominik.s.meister@gmail.com>
2024-11-17 16:31:20 +0100

    fixed table names

--- api/index.ts
+++ api/index.ts
@@ -82,14 +82,14 @@ app.post("/register", async (req: Request, res: Response) => {
 
     try {
         const existingUser =
-            await sql`SELECT * FROM users WHERE username = ${username}`;
+            await sql`SELECT * FROM snp_users WHERE username = ${username}`;
         if (existingUser.rows.length > 0) {
             return res.status(400).json({ message: "Username already exists" });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        await sql`INSERT INTO users (username, hashed_password) VALUES (${username}, ${hashedPassword})`;
+        await sql`INSERT INTO snp_users (username, hashed_password) VALUES (${username}, ${hashedPassword})`;
 
         res.status(201).json({ message: "User registered successfully" });
     } catch (err) {
